4cc7a626a379fd39e66d4b49e466c6b6ca0b3618
basusankha020-hash <basusankha020@gmail.com>
2026-01-10 22:56:24 +0530

    Add files via upload

---
+++ backend/config.py
@@ -0,0 +1,12 @@
+PROJECT_NAME = "AQI & PM2.5 Forecasting System"
+
+DATA_PERIOD = {
+    "start": "January 2022",
+    "end": "December 2024"
+}
+
+LAST_UPDATED = "January 2026"
+
+FORECAST_HORIZONS = ["24h", "48h", "72h"]
+
+DATA_SOURCE = "CPCB (Central Pollution Control Board, India)"
